48d3dfdd02dba6e7cceb6dbb27144f0c6bc7edc0
wilberg <wilberg@netrune.com>
2025-07-22 20:05:28 +0200

    Add FromRequestParts trait

--- src/request.rs
+++ src/request.rs
@@ -1,5 +1,6 @@
 use crate::response::IntoResponse;
 use crate::Response;
+use http::request::Parts;
 use hyper::body::Incoming;
 
 pub type Request = http::Request<Incoming>;
@@ -8,6 +9,12 @@ pub trait FromRequest<S>: Sized {
     type Error: IntoResponse + Send;
 
     fn from_request(request: Request, state: &S) -> impl Future<Output=Result<Self, Self::Error>> + Send;
+}
+
+pub trait FromRequestParts<S>: Sized {
+    type Error: IntoResponse + Send;
+
+    fn from_request_parts(parts: Parts, state: &S) -> impl Future<Output=Result<Self, Self::Error>> + Send;
 }
 
 impl<S> FromRequest<S> for Request
